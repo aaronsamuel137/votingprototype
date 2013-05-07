969d82eaeaf3f76720f8028280a289f5990fbef5
aaronsamuel137 <aaronsamuel137@gmail.com>
2013-05-06 22:09:53 -0600

    Added facebook login

---
+++ User.py
@@ -0,0 +1,15 @@
+#
+# User db model
+# 
+# Used for FB login
+
+from google.appengine.ext import db
+
+class User(db.Model):
+    id = db.StringProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    updated = db.DateTimeProperty(auto_now=True)
+    name = db.StringProperty(required=True)
+    profile_url = db.StringProperty(required=True)
+    access_token = db.StringProperty(required=True)
+    likes = db.TextProperty()
---
+++ util.py
@@ -0,0 +1,80 @@
+#
+# Commonly used utility functions
+#
+#
+
+import base64
+import hmac
+import random
+import string
+import time
+import hashlib
+import Cookie
+import email.utils
+import datetime
+
+FACEBOOK_APP_SECRET = "fe38d6dd8f118e20c99378f5151d28d1"
+
+### Useful functions ###
+
+def escape_html(s):
+    s = s.replace("&", "&amp;")
+    return cgi.escape(s, quote = True)
+
+
+
+def make_cookie(n):
+    return''.join(random.choice(string.letters + string.digits) for a in range(n))
+
+# adjust to our time zone
+def tz_adjust(entries):
+    for e in entries:
+        e.created = e.created + datetime.timedelta(hours = -6)
+
+
+def set_cookie(response, name, value, domain=None, path="/", expires=None):
+    """Generates and signs a cookie for the give name/value"""
+    timestamp = str(int(time.time()))
+    value = base64.b64encode(value)
+    signature = cookie_signature(value, timestamp)
+    cookie = Cookie.BaseCookie()
+    cookie[name] = "|".join([value, timestamp, signature])
+    cookie[name]["path"] = path
+    if domain:
+        cookie[name]["domain"] = domain
+    if expires:
+        cookie[name]["expires"] = email.utils.formatdate(
+            expires, localtime=False, usegmt=True)
+    response.headers.add("Set-Cookie", cookie.output()[12:])
+
+
+def parse_cookie(value):
+    """Parses and verifies a cookie value from set_cookie"""
+    if not value:
+        return None
+    parts = value.split("|")
+    if len(parts) != 3:
+        return None
+    if cookie_signature(parts[0], parts[1]) != parts[2]:
+        logging.warning("Invalid cookie signature %r", value)
+        return None
+    timestamp = int(parts[1])
+    if timestamp < time.time() - 30 * 86400:
+        logging.warning("Expired cookie %r", value)
+        return None
+    try:
+        return base64.b64decode(parts[0]).strip()
+    except:
+        return None
+
+
+def cookie_signature(*parts):
+    """Generates a cookie signature.
+
+    We use the Facebook app secret since it is different for every app (so
+    people using this example don't accidentally all use the same secret).
+    """
+    hash = hmac.new(FACEBOOK_APP_SECRET, digestmod=hashlib.sha1)
+    for part in parts:
+        hash.update(part)
+    return hash.hexdigest()
